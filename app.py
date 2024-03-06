e9eb354ed6219f8dfd555086702c2bd8e94c1c4e
Heloísa Britto <149850201+helsbritto@users.noreply.github.com>
2024-03-06 17:51:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ def list_locations():
     api_url = 'https://rickandmortyapi.com/api/location'
     response = requests.get(api_url)
 
-    if response.status_code == 100:
-    locations_data = response.json().get('results', [])
-        return render_template('locations.html', locations=locations_data)
+    if response.status_code == 200:
+        locations_data = response.json().get('results', [])
+        return render_template('profile.html', locations=locations_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
